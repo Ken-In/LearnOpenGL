5eed0786444e445657f049846f39a3c41d633981
Ken-In <yinfuquan0130@foxmail.com>
2023-01-08 08:36:12 +0800

    texture

--- LearnOpenGL/src/shaders/shader.fs
+++ LearnOpenGL/src/shaders/shader.fs
@@ -2,12 +2,20 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec2 Texcoord;
+
+uniform float mixValue;
 uniform vec4 timeColor;
+uniform sampler2D textureContainer;
+uniform sampler2D textureWall;
+uniform sampler2D textureFace;
+uniform sampler2D textureKeqing;
 
 
 void main()
 {
     //FragColor = vec4(ourColor, 1.0f);
-    FragColor = timeColor;
-
+    //FragColor = timeColor;
+    //vec2 invenseTex = vec2( 1.0 - Texcoord.x, Texcoord.y);
+    FragColor = mix(texture(textureFace, Texcoord), texture(textureKeqing, Texcoord), mixValue);
 }
