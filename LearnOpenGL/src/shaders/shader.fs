132109a22155e2a5ba22f5b11a6ec7c1f54b2203
Ken-In <yinfuquan0130@foxmail.com>
2023-01-11 09:39:03 +0800

    Lighting maps

--- LearnOpenGL/src/shaders/shader.fs
+++ LearnOpenGL/src/shaders/shader.fs
@@ -12,16 +12,14 @@ uniform sampler2D textureWall;
 uniform sampler2D textureFace;
 uniform sampler2D textureKeqing;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 cameraPos;
+uniform float matrixMove;
 
 struct Material 
 {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emission;
     float shininess;
 };
 uniform Material material;
@@ -38,19 +36,19 @@ uniform Light light;
 void main()
 {
     vec3 baseColor = texture(textureWall, Texcoord).xyz;
-    vec3 lightDir = normalize(lightPos - WorldPos.xyz);
+    vec3 lightDir = normalize(light.position - WorldPos.xyz);
     vec3 viewDir = normalize(cameraPos - WorldPos.xyz);
     vec3 norm = normalize(Normal);
 
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, Texcoord));
 
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, Texcoord));
 
     vec3 halfvec = normalize(lightDir + viewDir);
     float spec = pow(max(dot(halfvec, norm),0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular); 
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, Texcoord));
 
     vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0f);
+    FragColor = vec4(result, 1.0f) + texture(material.emission, vec2(Texcoord.x,Texcoord.y + matrixMove));
 }
