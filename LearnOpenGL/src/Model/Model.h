c29c85bf8a3b635b89efbfd5a42fb6f93265975b
Ken-In <yinfuquan0130@foxmail.com>
2023-01-12 18:56:36 +0800

    Model

---
+++ LearnOpenGL/src/Model/Model.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <assimp/scene.h>
+
+#include <map>
+
+#include "Mesh.h"
+
+class Model
+{
+public:
+	std::map<std::string, Texture> texture_loaded;
+	std::vector<Mesh> meshes;
+	std::string directory;
+
+	Model(std::string path)
+	{
+		loadModel(path);
+	}
+
+	void Draw(Shader& shader);
+
+private:
+	void loadModel(std::string& path);
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+};
