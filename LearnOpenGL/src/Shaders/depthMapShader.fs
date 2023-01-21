56187acda6ff16be587d5ee7e5aa0346ac067fa1
Ken-In <yinfuquan0130@foxmail.com>
2023-01-21 22:28:14 +0800

    Parallax Occlusion Mapping

--- LearnOpenGL/src/Shaders/depthMapShader.fs
+++ LearnOpenGL/src/Shaders/depthMapShader.fs
@@ -45,7 +45,9 @@ void main()
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 { 
     // number of depth layers
-    const float numLayers = 10;
+    const float minLayers = 8;
+    const float maxLayers = 32;
+    float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));
     // calculate the size of each layer
     float layerDepth = 1.0 / numLayers;
     // depth of current layer
@@ -68,5 +70,15 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
         currentLayerDepth += layerDepth;  
     }
 
-    return currentTexCoords;
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+
+    // get depth after and before collision for linear interpolation
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+
+    // interpolation of texture coordinates
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;
 }
